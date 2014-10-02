57d910d992cdf854c73f7076bebbc0e6ead50f8b
Orlando Karam <orlando.karam@gmail.com>
2014-10-02 03:10:31 +0000

    server

--- src/io/NetAdderServer.cs
+++ src/io/NetAdderServer.cs
@@ -1,6 +1,23 @@
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
 
 class AdderServer {
-      void AdderServer(int port) {
-           
-      }  
+  static void StartListeningOnPort(int port) {
+    TcpListener l=new TcpListener(IPAddress.Any,port);
+    while(true)
+      {
+        TcpClient c=l.AcceptTcpClient();
+        var data = new System.Byte[256];
+
+        // String to store the response ASCII representation.
+        string responseData = System.String.Empty;
+
+        // Read the first batch of the TcpServer response bytes.
+        int bytes = c.GetStream().Read(data, 0, data.Length);
+        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes-1);//skip the last \n
+        int sum=responseData.Split(' ').Select(x => int.Parse(x)).Sum();
+        c.GetStream().Write(
+      }
+  }  
 }
